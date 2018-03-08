062b8457094d788bc5b7b7ba6406311e4e5ff1c1
Chad Kerner <ckerner@illinois.edu>
2018-03-08 14:59:45 -0600

    added module to test settings.

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source colorize
+
+echo "${RED}I am RED${WHITE}"
+
+echo "${BOLD_RED}I am BOLD RED${WHITE}"
+
+echo "${UND_RED}I am UNDERLINED RED${WHITE}"
+
+echo "${BLACK}${BCK_YELLOW}I am BLACK with a YELLOW BACKGROUND${WHITE}"
+
